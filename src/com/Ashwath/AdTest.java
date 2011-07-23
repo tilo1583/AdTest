5937e6c80c320f6aa1ed061b1c57f333fa3ddbe7
Sadish <sadish.r@gmail.com>
2011-07-23 13:29:33 -0400

    Test Commit to Git

--- src/com/Ashwath/AdTest.java
+++ src/com/Ashwath/AdTest.java
@@ -31,7 +31,9 @@ public class AdTest extends Activity {
 	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        
+		//test
+		super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
         initializeInsults();
