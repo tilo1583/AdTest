c4a66a1ab116f9562c165d2c31c5072a826d7842
Tilottama Gaat <tilo1583@gmail.com>
2011-08-11 14:31:27 -0400

    Added background image devil.png and made ui changes in the layout

--- gen/com/Ashwath/R.java
+++ gen/com/Ashwath/R.java
@@ -11,11 +11,12 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int icon=0x7f020000;
+        public static final int devil=0x7f020000;
+        public static final int icon=0x7f020001;
     }
     public static final class id {
-        public static final int button3=0x7f060002;
-        public static final int button4=0x7f060003;
+        public static final int button3=0x7f060003;
+        public static final int button4=0x7f060002;
         public static final int linearlayout=0x7f060000;
         public static final int serif=0x7f060001;
     }
